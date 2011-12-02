e24e9d26a389511be752e1e86d5b3e7b43eb539c
77web <info@77-web.com>
2011-12-03 00:16:09 +0900

    add descriptin for this branch

--- web/setup.php
+++ web/setup.php
@@ -1,4 +1,11 @@
 <?php
+//WEBインストーラーの使い方
+//1.SNSを運用したい独自ドメインのドキュメントルートを/path/to/OpenPNE3/webに向くようにします。
+//2.http://(独自ドメイン)/setup.php/setupにアクセスします
+//3.フォームに必要事項を入力し、内容を確認してインストール実行ボタンをクリックします
+//4.インストール完了！（を目指している途中です…まだ未完成。）
+
+
 $configPath = dirname(__FILE__).'/../config/';
 $dummyPath = $configPath.'setup.txt';
 
